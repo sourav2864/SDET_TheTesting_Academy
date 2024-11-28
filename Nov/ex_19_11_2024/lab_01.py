d32741e73b6fab7193974f491f08b6b9b8b1b0c7
Sourav <sourav2864@gmail.com>
2024-11-28 18:58:04 +0530

    28_11_2024_task

---
+++ Nov/ex_19_11_2024/lab_01.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter the first num \n"))
+num2 = int(input("enter the second num \n"))
+num3 = int(input("enter the third num \n"))
+def SumTheNumber(n1="100",n2="200",n3="300"):
+    print(n1+n2+n3)
+
+SumTheNumber(num1,num2,num3)
